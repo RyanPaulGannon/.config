38c9999b82fa09c1e63d4a62cbac275e0f5ac597
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-05-22 22:06:06 +0100

    Update core keymaps

--- lua/ryanpaulgannon/core/keymaps.lua
+++ lua/ryanpaulgannon/core/keymaps.lua
@@ -2,3 +2,9 @@ local keymap = vim.keymap
 
 vim.g.mapleader = " "
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- windows
+keymap.set("n", "<S-n>", ":tabnew<CR>")
+keymap.set("n", "<S-t>", ":tabn<CR>")
+keymap.set("n", "<S-b>", ":tabp<CR>")
+keymap.set("n", "<S-c>", ":tabclose<CR>")
