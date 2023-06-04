d6900a2689afa60c317652b9fc89ecbe9450310f
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-04 16:40:02 +0100

    Comments in plugins-setup

--- nvim/lua/ryanpaulgannon/plugins-setup.lua
+++ nvim/lua/ryanpaulgannon/plugins-setup.lua
@@ -5,18 +5,27 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    -- Telescope
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
+    -- Treesitter
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
+    
+    -- Harpoon
     use('theprimeagen/harpoon')
+
+    -- Undotree
     use('mbbill/undotree')
+
+    -- Git
     use('tpope/vim-fugitive')
 
+    -- LSP
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -38,8 +47,10 @@ return require('packer').startup(function(use)
         }
     }
     
+    -- Theme
     use { "catppuccin/nvim", as = "catppuccin" }
 
+    -- Nvim-Tree
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
@@ -49,4 +60,5 @@ return require('packer').startup(function(use)
             require("nvim-tree").setup {}
         end
     }
+
 end)
