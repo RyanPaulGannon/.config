11d6768a32aa51c6ee364c513de2cdf3455e07d7
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-04 16:26:45 +0100

    Update dirs, plugins

--- lua/ryanpaulgannon/plugins-setup.lua
+++ lua/ryanpaulgannon/plugins-setup.lua
@@ -37,10 +37,7 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},     -- Required
         }
     }
-    use {
-        'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
+    
     use { "catppuccin/nvim", as = "catppuccin" }
 
     use {
