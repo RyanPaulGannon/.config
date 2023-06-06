0b01aefb0d6210901430932b5e5f0bc371ff31f7
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-06-06 22:12:27 +0100

    Keymap: Adjust NvimTreeToggle

--- nvim/lua/ryanpaulgannon/core/keymaps.lua
+++ nvim/lua/ryanpaulgannon/core/keymaps.lua
@@ -5,13 +5,13 @@ vim.g.mapleader = " "
 keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 
 -- Windows
-keymap.set("n", "<S-n>", ":tabnew<CR>")
-keymap.set("n", "<S-t>", ":tabn<CR>")
-keymap.set("n", "<S-b>", ":tabp<CR>")
-keymap.set("n", "<S-c>", ":tabclose<CR>")
+keymap.set("n", "<C-n>", ":tabnew<CR>")
+keymap.set("n", "<C-t>", ":tabn<CR>")
+keymap.set("n", "<C-b>", ":tabp<CR>")
+keymap.set("n", "<C-c>", ":tabclose<CR>")
 
 -- Tree Toggle
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>b", ":NvimTreeToggle<CR>")
 
 -- Yank to system clipboard
 keymap.set("n", "<leader>y", "\"+y")
