38545227777726f7f12816f817fe34688fbf4fdf
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-11-26 22:47:24 +0000

    `Main` Read data from json file

--- rmk/src/main.rs
+++ rmk/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct KeyboardData {
+    version: u8,
+    notes: String,
+    documentation: String,
+    keymap: String,
+    layout: String,
+    layers: Vec<Vec<String>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome, to RMK!");
+
+    if let Err(err) = get_keyboard() {
+        println!("{:?}", err);
+    }
+}
+
+fn get_keyboard() -> Result<(), Box<dyn std::error::Error>> {
+    let keyboard = fs::read_to_string("./rpg_crkbd_v1.json")?;
+    let keys: KeyboardData = serde_json::from_str(&keyboard)?;
+
+    for key in keys.layers {
+        println!("{:#?}", key);
+    }
+
+    Ok(())
 }
