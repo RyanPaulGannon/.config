c5f14c148492c7c11da23af91dc5cd92c515e399
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-12-08 09:24:00 +0000

    `nvim`: Require css from treesitter

--- nvim/lua/ryanpaulgannon/plugins/treesitter.lua
+++ nvim/lua/ryanpaulgannon/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "rust", "javascript", "typescript" },
+	ensure_installed = { "c", "lua", "rust", "javascript", "typescript", "css" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -11,7 +11,6 @@ require("nvim-treesitter.configs").setup({
 
 	highlight = {
 		enable = true,
-
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
