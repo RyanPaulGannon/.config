a1c4faa9d8f8ce77576b04665d26233a0311892c
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-12-09 19:59:50 +0000

    `nvim`: Update keymap for yank to clipboard

--- nvim/lua/ryanpaulgannon/core/keymaps.lua
+++ nvim/lua/ryanpaulgannon/core/keymaps.lua
@@ -2,10 +2,9 @@ local keymap = vim.keymap
 
 vim.g.mapleader = " "
 
-keymap.set('n', 'E', vim.cmd.Ex)
-keymap.set('n', '<C-s>', ':w<CR>')
+keymap.set("n", "E", vim.cmd.Ex)
+keymap.set("n", "<C-s>", ":w<CR>")
 
 -- Yank to system clipboard
-
-keymap.set("n", "<leader>y", "\"+y")
-
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
