4c9c695d8734e9624de0d99bd465de4ef2097a6c
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-12-13 07:07:35 +0000

    `Keymaps`: Update cmd's for harpoon and save

--- nvim/lua/ryanpaulgannon/core/keymaps.lua
+++ nvim/lua/ryanpaulgannon/core/keymaps.lua
@@ -3,7 +3,7 @@ local keymap = vim.keymap
 vim.g.mapleader = " "
 
 keymap.set("n", "E", vim.cmd.Ex)
-keymap.set("n", "<C-s>", ":w<CR>")
+keymap.set("n", "<C-w>", ":w<CR>")
 
 -- Yank to system clipboard
 keymap.set({ "n", "v" }, "<leader>y", [["+y]])
