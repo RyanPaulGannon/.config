9a05dba6d752affce8ca7e7e0ad63169b4fb1031
Ryan Gannon <ryanpaulgannon@icloud.com>
2024-01-10 06:41:13 +0000

    `Harpoon`: Updated commands

--- nvim/lua/ryanpaulgannon/plugins/harpoon.lua
+++ nvim/lua/ryanpaulgannon/plugins/harpoon.lua
@@ -1,32 +1,60 @@
 return {
-	"ThePrimeagen/harpoon",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+	"theprimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local keymap = vim.keymap
-		local harpoon = require("harpoon")
-		harpoon.setup({
-			keymap.set("n", "<leader>a", function()
-				harpoon:list():append()
-			end),
-
-			keymap.set("n", "<leader>h", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end),
-
-			keymap.set("n", "<C-h>", function()
-				harpoon:list():select(1)
-			end),
-			keymap.set("n", "<C-t>", function()
-				harpoon:list():select(2)
-			end),
-			keymap.set("n", "<C-n>", function()
-				harpoon:list():select(3)
-			end),
-			keymap.set("n", "<C-s>", function()
-				harpoon:list():select(4)
-			end),
-		})
+		require("harpoon"):setup()
 	end,
+	keys = {
+		{
+			"<leader>a",
+			function()
+				require("harpoon"):list():append()
+			end,
+			desc = "Add file to Harpoon",
+		},
+		{
+			"<leader>h",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Harpoon UI",
+		},
+		{
+			"<leader>1",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "Harpoon file one",
+		},
+		{
+			"<leader>2",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "Harpoon file two",
+		},
+		{
+			"<leader>3",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "Harpoon file three",
+		},
+		{
+			"<leader>4",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "Harpoon file four",
+		},
+		{
+			"<leader>5",
+			function()
+				require("harpoon"):list():select(5)
+			end,
+			desc = "Harpoon file five",
+		},
+	},
 }
