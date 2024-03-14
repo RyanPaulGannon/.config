f913d0c7e0e2ba6559412bc9bf101ef5eba7b92c
Ryan Gannon <ryanpaulgannon@icloud.com>
2024-03-14 12:18:04 +0000

    `Corne`: Move to 36 keys

--- crkbd/ryanpaulgannon/keymap.c
+++ crkbd/ryanpaulgannon/keymap.c
@@ -27,20 +27,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, KC_LGUI,  KC_SPC,     KC_ENT, KC_BSPC,   TO(1)
+                                          XXXXXXX, KC_LCTL,  KC_SPC,     KC_ENT, TO(1), XXXXXXX
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     XXXXXXX,    KC_ESC,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9, KC_BSPC, XXXXXXX,
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------
+     XXXXXXX, KC_ESC,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9, KC_BSPC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_TAB, XXXXXXX, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,  XXXXXXX,  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+     XXXXXXX, KC_TAB, XXXXXXX, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP,              XXXXXXX,  KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,  KC_0, KC_1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, TO(0),  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          XXXXXXX, TO(0),  KC_LGUI,     KC_ENT,   TO(2), XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -50,6 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TAB, KC_EXLM, KC_AT, LSFT(KC_3),  KC_DLR, KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,   KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INT1,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, TO(3), KC_RALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+    [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB, KC_EXLM, KC_AT, LSFT(KC_3),  KC_DLR, KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL, KC_MS_LEFT, XXXXXXX, XXXXXXX, KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,   KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INT1,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
