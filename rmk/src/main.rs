c3fd17deb8bf5b9594baf0f5e06d9b7033e28582
Ryan Gannon <ryanpaulgannon@icloud.com>
2024-03-31 18:13:41 +0100

    `RMK`: Stripped back to basic setup for RP2040

--- rmk/src/main.rs
+++ rmk/src/main.rs
@@ -1,32 +1,10 @@
-use std::fs;
+#![no_std]
+#![no_main]
 
-use serde::Deserialize;
+use cortex_m_rt::entry;
+use panic_halt as _;
 
-#[derive(Deserialize, Debug)]
-struct KeyboardData {
-    version: u8,
-    notes: String,
-    documentation: String,
-    keymap: String,
-    layout: String,
-    layers: Vec<Vec<String>>,
-}
-
-fn main() {
-    println!("Welcome, to RMK!");
-
-    if let Err(err) = get_keyboard() {
-        println!("{:?}", err);
-    }
-}
-
-fn get_keyboard() -> Result<(), Box<dyn std::error::Error>> {
-    let keyboard = fs::read_to_string("./rpg_crkbd_v1.json")?;
-    let keys: KeyboardData = serde_json::from_str(&keyboard)?;
-
-    for key in keys.layers {
-        println!("{:#?}", key);
-    }
-
-    Ok(())
+#[entry]
+fn main() -> ! {
+    loop {}
 }
